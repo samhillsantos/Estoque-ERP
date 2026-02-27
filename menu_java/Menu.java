488391a528df1fce0d6c8413be2dd41ec96d1294
samhillsantos <seu-email@exemplo.com>
2026-02-27 17:45:41 -0300

    Estrutura inicial de glapoes vazios e vizualização

--- menu_java/Menu.java
+++ menu_java/Menu.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Menu{
     // Declaraçoes globais
     public static Scanner input = new Scanner(System.in);
+    public static Armazem armazem = new Armazem();
+    public static Armazem[] galpoes = new Armazem[10];
     
     // Main
-    public static void main(String[] args){
+        public static void main(String[] args){
         System.out.println("Bem vindo ao ERP Estoque");
+        System.out.println(java.util.Arrays.toString(galpoes));
     }
     
     // Funçōes
